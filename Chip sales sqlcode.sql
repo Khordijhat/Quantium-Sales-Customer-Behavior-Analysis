26645a35ded434e0371b875a1681951c81a02dce
Adijat <oluwatoyinfajobi2004@gmail.com>
2026-08-29 10:24:44 +0100

    sql_code

---
+++ Chip sales sqlcode.sql
@@ -0,0 +1,32 @@
+SELECT Transaction_date,product_name,P.loyalty_card_number,TXN,Store_nbr,product_number,product_quantity,Total_sales,
+TRIM(CONCAT(REGEXP_REPLACE(REGEXP_REPLACE(product_name,'swt pot','sweet potatoes') ,'[0-9]+g',''),'',' ',REGEXP_SUBSTR(REGEXP_REPLACE(product_name,'swt pot','sweet potatoes') ,'[0-9]+g'))) AS  Standard_product_name,
+CASE 
+WHEN product_name LIKE 'Dorit%'THEN 'Doritos'
+WHEN product_name LIKE 'GrnWves%' THEN 'Grain Waves'
+WHEN product_name LIKE 'Infz%' THEN 'Infuzions'
+WHEN product_name LIKE 'NNC%' THEN 'Natural Chip Co'
+WHEN product_name LIKE 'RRD%' THEN 'Red Rock Deli'
+WHEN  product_name LIKE 'smith%' THEN 'Smiths'
+WHEN product_name LIKE 'sn%' THEN 'Sunbites'
+WHEN Product_name LIKE 'ww%' THEN 'Woolworths'
+ELSE SUBSTRING_INDEX(product_name,' ' ,1) 
+END AS Brand_name,
+REGEXP_SUBSTR(REGEXP_REPLACE(product_name,'swt pot','sweet potatoes') ,'[0-9]+g') AS weight_size,
+CASE WHEN CAST(REGEXP_SUBSTR(product_name ,'[0-9]+g') AS UNSIGNED) <=150 THEN 'Low weight'
+WHEN CAST(REGEXP_SUBSTR(product_name ,'[0-9]+g')AS UNSIGNED) <=250 THEN 'Medium weight'
+ELSE 'High weight'
+END AS Weight_category,lifestage,premium_customer
+FROM Transaction_data T
+JOIN purchased_behaviour P
+	ON T.loyalty_card_number=P.loyalty_card_number;
+
+
+
+
+
+
+
+
+
+
+
